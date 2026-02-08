fecd39f3b6b17baad0df059307b5bc2f621a9361
davidzapatab05 <davidzapata.dz051099@gmail.com>
2026-02-08 02:00:15 -0500

    feat: improve accessibility and LCP performance

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -34,6 +34,7 @@ export function Navbar() {
                         width={40}
                         height={40}
                         className="h-10 w-auto object-contain"
+                        priority
                     />
                     <span className="text-xl font-bold text-primary tracking-tight hidden sm:block">Danchin</span>
                 </Link>
